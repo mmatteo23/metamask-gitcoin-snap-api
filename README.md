983dd042b614f7efcb5639b611a8ae7462423459
Matteo Midena <75645114+mmatteo23@users.noreply.github.com>
2024-02-12 23:06:35 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Basic Node.js Backend template
+This template is a basic service with:
+- Node.js
+- Express.js
+- Nodemon
+- Typescript
---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express, { Express, Request, Response , Application } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+//For env File 
+dotenv.config();
+
+const app: Application = express();
+app.use(express.json());
+app.use(cors());
+
+const port = process.env.PORT || 8000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Welcome to Express & TypeScript Server');
+});
+
+app.listen(port, () => {
+  console.log(`Server is Fire at http://localhost:${port}`);
+});
